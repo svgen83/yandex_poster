a1b2b0a3d9da8d18619ea00346bb6d08149c063c
svg83 <svgeneraloff@gmail.com>
2023-04-28 16:22:51 +0400

    подключил статику

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+##STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 TEMPLATES = [
     {
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
 from mysite import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
