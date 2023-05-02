033989ef323d0344a851bc9be6d6e45ebdae18f5
svg83 <svgeneraloff@gmail.com>
2023-05-02 14:15:41 +0400

    add Agency model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
 # Create your models here.
-print("hello!")
+class Agency(models.Model):
+    title = models.CharField('Название организации', max_length=200)
+    description_short = models.TextField('Краткое описание')
+    description_long = models.TextField('Подробное описание')
+    latitude = models.FloatField(null=True, blank=True,
+                                 verbose_name='широта')
+    longitude = models.FloatField(null=True, blank=True,
+                                  verbose_name='долгота')
+
+    def __str__(self):
+        return self.title
