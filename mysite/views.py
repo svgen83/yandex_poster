913bcbf548f696f4ea38d0315892a3bbafa625d7
svg83 <svgeneraloff@gmail.com>
2023-05-10 13:02:50 +0400

    Move data from template to view

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from places.models import Agency
 
 
 def index(request):
-   return render(request, 'index.html')
+   places = Agency.objects.all()
+   agency_places = []
+   for place in places:
+      agency_places.append(
+         {"type": "Feature",
+         "geometry": {
+            "type": "Point",
+            "coordinates": [place.longitude, place.latitude]},
+          "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            #"detailsUrl": "./places/moscow_legends.json"
+            }}
+         )
+   data = {"places_geojson": {
+      "type": "FeatureCollection",
+      "features": agency_places}}
+   return render(request, "index.html", context=data)
