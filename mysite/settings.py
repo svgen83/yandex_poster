80b715f731e08e95e204182eb6f790564cc11e83
svgen83 <61458549+svgen83@users.noreply.github.com>
2023-07-18 16:06:49 +0400

    fix STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,11 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-##STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATICFILES_DIRS = [BASE_DIR / 'static']
 
-##MEDIA_ROOT = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
