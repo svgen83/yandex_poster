9a6f96bdc919ebe1bab48363ef124826196ae779
svg83 <svgeneraloff@gmail.com>
2023-07-31 13:49:48 +0400

    split def_get_images

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -6,34 +6,22 @@ from django.core.files.base import ContentFile
 from places.models import Place, Picture
 
 
-def get_or_create_content(place_description):
-    place, created = Place.objects.get_or_create(
-        title=place_description['title'],
-        description_short=place_description['description_short'],
-        description_long=place_description['description_long'],
-        latitude=place_description['coordinates']['lat'],
-        longitude=place_description['coordinates']['lng'])
-    logging.warning(f'Создан новый объект "{place_description["title"]}"')
-    return place, created
-
-
-def get_place_description(url):
-    response = requests.get(url)
+def download_image_content(index, img_url):
+    response = requests.get(img_url)
     response.raise_for_status()
-    return response.json()
+    return {'content': response.content,
+            'image_name': img_url.split('/')[-1],
+            'image_number': index}
 
 
-def get_images(img_urls, place):
-    for index, img_url in enumerate(img_urls):
-        image_name = img_url.split('/')[-1]
-        response = requests.get(img_url)
-        response.raise_for_status()
-        image_content = ContentFile(response.content, name=image_name)
-        Picture.objects.create(
-            image=image_content,
-            sight=place,
-            number=index)
-    logging.warning('Картинки загружены')
+def upload_image(image_content, place_title):
+    upload_content = ContentFile(
+        image_content['content'],
+        name=image_content['image_name'])
+    Picture.objects.create(
+        image=upload_content,
+        sight=place_title,
+        number=image_content['image_number'])
 
 
 class Command(BaseCommand):
@@ -41,10 +29,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         url = options['url']
-        place_description = get_place_description(url)
-        place, created = get_or_create_content(place_description)
+        response = requests.get(url)
+        response.raise_for_status()
+        place_description = response.json()
+        place_title, created = Place.objects.get_or_create(
+            title=place_description['title'],
+            description_short=place_description['description_short'],
+            description_long=place_description['description_long'],
+            latitude=place_description['coordinates']['lat'],
+            longitude=place_description['coordinates']['lng'])
         if created:
-            get_images(place_description['imgs'], place)
+            logging.warning(f'Создан новый объект "{place_description["title"]}"')
+            for index, img_url in enumerate(place_description['imgs']):
+                image_content = download_image_content(index, img_url)
+                upload_image(image_content, place_title)
+            logging.warning('Картинки загружены')
+            
 
     def add_arguments(self, parser):
         parser.add_argument('url', type=str, help='json url')
